3d1cff5f359c039db1492b9db8baf526501993ec
Sofia de Alcantara <sofiaalcantaras@outlook.com>
2024-10-22 12:44:39 -0300

    Delete JAVA_CalculadoraReflection directory

--- JAVA_CalculadoraReflection/src/br/ifes/projetosistemas/calculadora/model/operation/Multiplicar.java
+++
@@ -1,8 +0,0 @@
-package br.ifes.projetosistemas.calculadora.model.operation;
-
-public class Multiplicar implements IOperation{
-
-    public int calculate(int valor1, int valor2){
-        return valor1 * valor2;
-    }
-}
